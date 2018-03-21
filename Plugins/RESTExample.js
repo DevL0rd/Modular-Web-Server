abe9d8cb4f61187ef97376e98be43189a4768093
DevL0rd <dmhzmxn@gmail.com>
2018-03-21 15:31:54 -0500

    Patched up security issues, fixed byte-rangelimit, fixed max post size and url length

--- Plugins/RESTExample.js
+++ Plugins/RESTExample.js
@@ -3,7 +3,7 @@
 //Last Update: 3/17/2018
 //Version: 1.0.0
 function init(settings, events, io, log, commands) {
-    events.on("post", function (request, response, body) {
+    events.on("post", function (request, response, urlParts, body) {
         response.writeHead(200, {
             'Content-Type': 'application/json'
         });
@@ -11,6 +11,19 @@ function init(settings, events, io, log, commands) {
             data: "post received"
         }
         response.end(JSON.stringify(reponseData));
+        //Tell server the request was handled by this plugin
+        return true;
+    })
+    events.on("get", function (request, response, urlParts, ispath) {
+        response.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        var reponseData = {
+            data: "post received"
+        }
+        response.end(JSON.stringify(reponseData));
+        //Tell server the request was handled by this plugin
+        return true;
     })
 }
 
